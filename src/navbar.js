67175347deb677d9647779a0139dc7d2ea8d3818
3Melody <plugeiei2003@gmail.com>
2023-09-17 17:34:42 +0700

    final navbar

--- src/navbar.js
+++ src/navbar.js
@@ -15,3 +15,20 @@ document.addEventListener('click', function (event) {
         dropdown.style.display = 'none';
     }
 });
+
+const dropdownservice = document.querySelector('.service-system');
+const dropdownserviceid = document.querySelector('#id-dropdown-service');
+
+dropdownserviceid.addEventListener('click', () => {
+    if (dropdownservice.style.display === 'none') {
+        dropdownservice.style.display = 'block';
+    } else {
+        dropdownservice.style.display = 'none';
+    }
+}); 
+
+document.addEventListener('click', function (event) {
+    if (!dropdownserviceid.contains(event.target) && !dropdownservice.contains(event.target)) {
+        dropdownservice.style.display = 'none';
+    }
+});
